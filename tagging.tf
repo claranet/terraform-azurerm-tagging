d4bc91d2aa493eaa3b9df6ecee3bb1e48211d9aa
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2020-01-15 16:34:45 +0100

    AZ-94 fmt

--- tagging.tf
+++ tagging.tf
@@ -11,20 +11,20 @@ resource "null_resource" "tags" {
   # Code found here https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-using-tags#code-try-22
   provisioner "local-exec" {
     command = <<CMD
-      ${templatefile("${path.module}/script/tag.sh.tmpl", {behavior = var.behavior,
-                                            resource_id = var.resource_ids[count.index],
-                                            tags = local.tags,
-                                            subscription_id = data.azurerm_client_config.current.subscription_id
-                                            }
-                    )
-        }
+      ${templatefile("${path.module}/script/tag.sh.tmpl", { behavior = var.behavior,
+    resource_id     = var.resource_ids[count.index],
+    tags            = local.tags,
+    subscription_id = data.azurerm_client_config.current.subscription_id
+  }
+)
+}
     CMD
-  }
+}
 
-  triggers = {
-    resource    = var.resource_ids[count.index]
-    tags        = jsonencode(var.tags)
-    force-apply = var.force ? uuid() : false
-    behavior    = var.behavior
-  }
+triggers = {
+  resource    = var.resource_ids[count.index]
+  tags        = jsonencode(var.tags)
+  force-apply = var.force ? uuid() : false
+  behavior    = var.behavior
+}
 }
