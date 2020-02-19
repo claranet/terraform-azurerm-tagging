54b74bf38678b487f9d50921e8239ec0d75f47fb
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-02-19 11:40:00 +0100

    AZ-65: Fixup args value escape, cleaner HCL2 code, requires azure-cli >= 2.1

--- tagging.tf
+++ tagging.tf
@@ -1,5 +1,5 @@
 locals {
-  tags = join(" ", formatlist("%s=%s", keys(var.tags), values(var.tags)))
+  tags = join(" ", formatlist("%s=\"%s\"", keys(var.tags), values(var.tags)))
 }
 
 data "azurerm_client_config" "current" {}
@@ -8,23 +8,24 @@ resource "null_resource" "tags" {
 
   count = var.nb_resources
 
+  triggers = {
+    resource    = var.resource_ids[count.index]
+    tags        = jsonencode(var.tags)
+    force-apply = var.force ? uuid() : false
+    behavior    = var.behavior
+  }
+
   # Code found here https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-using-tags#code-try-22
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command = <<CMD
-      ${templatefile("${path.module}/script/tag.sh.tmpl", { behavior = var.behavior,
-    resource_id     = var.resource_ids[count.index],
-    tags            = local.tags,
-    subscription_id = data.azurerm_client_config.current.subscription_id
+    command = templatefile("${path.module}/script/tag.sh.tmpl",
+      {
+        behavior        = var.behavior
+        resource_id     = var.resource_ids[count.index]
+        tags            = local.tags
+        subscription_id = data.azurerm_client_config.current.subscription_id
+      }
+    )
   }
-)}
-    CMD
-}
 
-triggers = {
-  resource    = var.resource_ids[count.index]
-  tags        = jsonencode(var.tags)
-  force-apply = var.force ? uuid() : false
-  behavior    = var.behavior
-}
 }
