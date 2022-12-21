84a914a4aa20b1be219c52dcd18ea74ba8ddd1bd
epopisces <epopisces@gmail.com>
2022-12-21 14:37:54 -0600

    interpreter var/validtn, if logic for PS, PS tmpl

--- variables.tf
+++ variables.tf
@@ -18,9 +18,23 @@ variable "behavior" {
   description = "Behavior for tags applying. Must be `merge` or `overwrite`."
   type        = string
   default     = "merge"
+  validation {
+    condition     = contains(["merge", "overwrite"], var.behavior)
+    error_message = "The value for behavior must be either `merge` or `overwrite`."
+  }
 }
 
 variable "nb_resources" {
   description = "Number of resources to tag"
   type        = number
+}
+
+variable "interpreter" {
+  description = "Indicates which OS the script is running in"
+  type        = string
+  default     = "bash"
+  validation {
+    condition     = contains(["bash", "PowerShell"], var.interpreter)
+    error_message = "The value for interpreter must be either `bash` or `PowerShell`."
+  }
 }
