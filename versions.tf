a880074971779a0ffce40ff3eed0a327eed66906
Jérôme Respaut <jerome.respaut@claranet.com>
2025-04-18 14:29:29 +0200

    feat!: upgrade module to v7

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.3"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 1.31"
+      version = ">= 3.0"
     }
     null = {
       source  = "hashicorp/null"
